baede636b549476ac8e0d6bbe97585a4a0be3c71
unknown <billbrown20000@gmail.com>
2020-03-30 15:43:33 -0400

    assessment Complete

--- src/main/java/Assessment1/AssessmentTester.java
+++ src/main/java/Assessment1/AssessmentTester.java
@@ -7,15 +7,18 @@ public class AssessmentTester {
 
 
         Student BillyTheKiDD = new Student("William ", "Brown");
-        Scores Grades = new Scores();
 
 
 
         System.out.print(BillyTheKiDD.getFirstName());
         System.out.print(BillyTheKiDD.getLastName());
-        System.out.println(Arrays.toString(Grades.getScores()));
-        System.out.println(Grades.calculateGradeAverage(67.0,77.9,89.0));
-        System.out.println(Grades.totalScores(67.0,77.9,89.0));
+        System.out.println(Arrays.toString(BillyTheKiDD.getScores()));
+        System.out.println(Arrays.toString(BillyTheKiDD.getScores()));
+        System.out.println(BillyTheKiDD.calculateGradeAverage(67.0,77.9,89.0));
+        System.out.println(BillyTheKiDD.totalScores(67.0,77.9,89.0));
+
+
+
 
 
 
--- src/main/java/Assessment1/Student.java
+++ src/main/java/Assessment1/Student.java
@@ -3,8 +3,13 @@ package Assessment1;
 public class Student {
     private String firstName;
     private String lastName;
+   private double scores[] = {67.0, 77.9, 89.0};
 
-    public Student (String firstName, String lastName){
+    public Student(){
+
+    }
+
+    public Student(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
@@ -13,15 +18,28 @@ public class Student {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public double[] getScores() {
+        return scores;
+    }
+
+    public double calculateGradeAverage(double average, double v, double Average) {
+        int sum = 0;
+        for (int x = 0; x < scores.length; x++)
+            sum += scores[x];
+        Average = sum / scores.length;
+        return Average;
+    }
+    public double totalScores(double sum1, double v, double sum){
+        sum = 0;
+        for (int x = 0; x < scores.length; x++)
+            sum += scores[x];
+        return sum;
     }
 }
+
+
